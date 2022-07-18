e3fc3a5209c6c06b30267b4278c648ae0a1d49e3
dharmik <dharmikvaishnav0419@gmail.com>
2022-07-18 09:40:00 +0530

    commit

---
+++ src/comp/SignUp.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const SignUp = () => {
+  return (
+    <>
+    <h5>sign up </h5>
+    <form>
+<input  type="text"></input>
+<input type="text"></input>
+<input type="text"></input>
+    </form>
+    </>
+  )
+}
+
+export default SignUp
---
+++ src/comp/user/UserSlice.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+
+
+const UserSlice = ({
+    name: 'user',
+    initialState,
+    reducers:{
+        
+    }
+})
+
+export const {} = UserSlice.actions
+export default UserSlice.reducer;
